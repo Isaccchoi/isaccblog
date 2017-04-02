c52a250fa7336678b99803da50c66ccd952b7db5
isaccchoi <isaccchoi@naver.com>
2017-04-02 22:46:42 +0900

    create postlist view

--- post/models.py
+++ post/models.py
@@ -10,21 +10,31 @@ class Category(models.Model):
         return self.name
 
 
+PERMISSIBLE_RANGE_CHOICE = (
+    ("onlyme", "나에게만"),
+    ("friends", "친구만"),
+    ("public", "모두에게"),
+)
+
+
 class Post(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, null=False)
     category = models.ForeignKey(Category)
     title = models.CharField(max_length=120)
     content = models.TextField()
-    tags = models.ManyToManyField('tag')
+    tags = models.ManyToManyField("tag")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+    # permissbile_range = models.CharField(max_length=120,
+                            # choice=PERMISSIBLE_RANGE_CHOICE)
     # image
-    
+
     def __str__(self):
         return self.title
 
     class Meta:
-        ordering = ['-updated_at',]
+        ordering = ["-updated_at",]
 
 
 class Comment(models.Model):
@@ -35,7 +45,7 @@ class Comment(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-        ordering = ['-updated_at',]
+        ordering = ["-updated_at",]
 
 
 class Tag(models.Model):
